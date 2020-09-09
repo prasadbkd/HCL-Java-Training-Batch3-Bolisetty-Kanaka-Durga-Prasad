d456a03cd5d91a269d010a94946e374c035a3e17
Prasad BKD <48713377+prasadbkd@users.noreply.github.com>
2020-09-10 00:52:10 +0530

    Delete Shivgadh.java

--- Interfaces(HandsOn-5)/src/com/hc/example5/Shivgadh.java
+++
@@ -1,9 +0,0 @@
-package com.hc.example5;
-
-public class Shivgadh implements Fort {
-	@Override
-	public void distance() {
-
-		System.out.println("You are going to visit Shivgadh\r\n" + "The distance is 100 km\r\n");
-	}
-}
